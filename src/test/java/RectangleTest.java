5586b8aac85314570f38c7c3ec28f1154bef778d
Courtney Phillips <courtney@epicodus.com>
2016-09-06 16:35:17 -0700

    Write test to confirm that constructor records length value, and getLength() method successfully returns it.

--- src/test/java/RectangleTest.java
+++ src/test/java/RectangleTest.java
@@ -10,7 +10,7 @@ public class RectangleTest {
   }
 
   @Test
-  public void newRectangle_savesLength_4() {
+  public void newRectangle_getsLength_4() {
     Rectangle testRectangle = new Rectangle(2, 4);
     assertEquals(4, testRectangle.getLength());
   }
