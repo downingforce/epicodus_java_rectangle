994559158225d43769fe2b4a68eb850dd935cddb
Courtney Phillips <courtney@epicodus.com>
2016-09-06 15:58:05 -0700

    Write test to assert that we can successfully create new Rectangle objects.

--- src/test/java/RectangleTest.java
+++ src/test/java/RectangleTest.java
@@ -3,4 +3,10 @@ import static org.junit.Assert.*;
 
 public class RectangleTest {
 
+  @Test
+  public void newRectangle_instantiatesCorrectly() {
+    Rectangle testRectangle = new Rectangle(2, 4);
+    assertEquals(true, testRectangle instanceof Rectangle);
+  }
+
 }
