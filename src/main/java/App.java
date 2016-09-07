b3af13aa3652686320656e7dd48e002bda82be58
Courtney Phillips <courtney@epicodus.com>
2016-09-06 17:18:27 -0700

    Parse user-provided length from string into int.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -4,6 +4,7 @@ public class App {
   public static void main(String[] args) {
     Console myConsole = System.console();
     System.out.println("Enter the length of your rectangle:");
-
+    String stringLength = myConsole.readLine();
+    int intLength = Integer.parseInt(stringLength);
   }
 }
