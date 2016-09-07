9f8fe9c259c6305c55d7d3c29023f2ce66f9822c
Courtney Phillips <courtney@epicodus.com>
2016-09-06 17:17:31 -0700

    Prompt users to enter rectangle length value.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,5 +3,7 @@ import java.io.Console;
 public class App {
   public static void main(String[] args) {
     Console myConsole = System.console();
+    System.out.println("Enter the length of your rectangle:");
+
   }
 }
